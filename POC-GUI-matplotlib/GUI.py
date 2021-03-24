d13f980bb0f545e4ce1ee7b459093faf9d1689df
mickael <you@example.com>
2021-03-24 22:53:56 +0100

    GUI by matplotlib

---
+++ POC-GUI-matplotlib/GUI.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+
+cmap = ListedColormap(["black", "white"]) #Colors chosen for the Game
+fig, ax = plt.subplots() 
+fig.canvas.set_window_title("Game Of Life by Mickael")
+
+ax.axis('off')
+for i in range(10):
+    v = np.random.randint(2, size=(50, 50)) #Random generation of matrix 
+    ax.matshow(v, cmap=cmap)
+    plt.draw()
+    plt.pause(1)
+plt.show(block=True) #Block plot at the end
+
