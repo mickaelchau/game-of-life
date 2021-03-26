748fe34982586eb8884669d79cb1e69de5a7d1ee
mickael <you@example.com>
2021-03-26 05:14:25 +0100

    text is now visible even if only one cell, text color change to red

--- process.py
+++ process.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt                                                 
-from matplotlib.colors import ListedColormap    
+from matplotlib.colors import ListedColormap, BoundaryNorm    
 def Input():
     size = int(input("Enter size of Board: "))
     if (size <= 0):
@@ -30,17 +30,28 @@ def Init():
     return size, pos, steps, duration
 
 def Game(board, n, wait=1):                                          
+    #There is the creation of GUI
     cmap = ListedColormap(["white", "black"]) #Colors chosen for the Game       
+    bounds = [0, 1, 2]
+    norm = BoundaryNorm(bounds, cmap.N)
+
     fig, ax = plt.subplots()
     fig.canvas.set_window_title("Game Of Life by Mickael")                      
-    text = ax.text(0, 0, "")
     ax.axis('off')                                                             
+
+    text = ax.text(0, 0, "")
+    text.set_color("red")
+    text.set_zorder(1.)
+
     v = board.matrix
     maxIter = str(n)
     for i in range(n):                                                          
+        #There is sugar syntax to print dynamic text 
         infoInter = "Iterations: " + str(i+1) + "\nFinal iteration: " + maxIter
         text.set_text(infoInter)
-        ax.matshow(v, cmap=cmap)                                                
+
+        #Refresh figure and axes 
+        ax.matshow(v, cmap=cmap, norm=norm)                                                
         plt.pause(wait)                                                         
         board.play()
         fig.canvas.draw()
